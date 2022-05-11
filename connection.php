77eef266b35cf888fdc89b6d2ae33f1a13aa9075
piesimon <99826646+piesimon@users.noreply.github.com>
2022-05-11 09:14:57 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'company';
+$user ='root';
+$pass ='';
+$charset= 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$option =[
+PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
+PDO::ATTR_EMULATE_PREPARES    =>false ,];
+
+try{
+    $connection = new PDO($dsn, $user, $pass, $option);
+}
+catch(PDOexception $e)
+{
+    throw new PDOexception($e->getMessage(), (int)$e->getCode());
+} 
